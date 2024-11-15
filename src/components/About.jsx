ae28d438f60d033a862aeeb6bf652d2707b04a28
rikster-r <seitdanial43@gmail.com>
2024-11-16 00:45:23 +0500

    Update about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { Player } from "@lottiefiles/react-lottie-player";
-import { motion } from "framer-motion";
-import SectionTitle from "./SectionTitle";
+import React from 'react';
+import { Player } from '@lottiefiles/react-lottie-player';
+import { motion } from 'framer-motion';
+import SectionTitle from './SectionTitle';
 
 const About = () => {
   return (
@@ -19,7 +19,7 @@ const About = () => {
           autoplay
           loop
           src="https://assets10.lottiefiles.com/private_files/lf30_esg1l8r1.json"
-          style={{ height: "100%" }}
+          style={{ height: '100%' }}
           className="w-[250px] h-[250px] md:w-[350px] md:h-[350px] 2xl:w-[550px] 2xl:h-[550px]"
         />
       </motion.div>
@@ -31,7 +31,22 @@ const About = () => {
           whileInView={{ y: 0, opacity: 1 }}
           viewport={{ once: true }}
         >
-          I'm a full-stack web developer who enjoys turning ideas into functional, user-friendly websites. I work with both front-end and back-end technologies to create projects that look good and work smoothly. I like to keep things simple and focus on delivering what users really need. I’m always looking to improve my skills and take on interesting projects. Let’s connect if you’d like to work together.
+          I’m a full-stack web developer who enjoys turning ideas into
+          functional, user-friendly websites. I work with front-end and back-end
+          technologies to create projects that look great and work smoothly. I
+          like keeping things simple and making sure users get exactly what they
+          need.
+        </motion.p>
+        <motion.p
+          className="text-lg 2xl:text-2xl mx-0 sm:mx-4 lg:mx-16 xl:mx-32 mt-4"
+          initial={{ y: 150, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          viewport={{ once: true }}
+        >
+          Outside of work, I stay active at the gym, express my creativity
+          through style, and unwind by singing. These hobbies keep me inspired,
+          disciplined, and ready to bring fresh ideas to every project I tackle.
+          Let’s connect and build something great together!
         </motion.p>
       </div>
     </section>
